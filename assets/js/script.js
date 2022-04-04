0560694541e74ea145525f2c9400a3143c2759ab
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-04-03 22:25:13 -0500

    modified the js file to see if there was a differenve using let vs var.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,7 @@ $("#currentDay")
 // retrieve class with hours and change to past present or future using css
 $(".hour").each(function(){
     var blockTime = parseInt($(this).attr("id"));
-    //console.log(this);
-    //console.log($(".hour"))
+    console.log(this);
 
     // current hour
     var hourNow = moment().hour();
@@ -15,9 +14,11 @@ $(".hour").each(function(){
 
     // get item from local storage
     var textToDo = localStorage.getItem(blockTime);
+    //console.log(textToDo);
 
-    if(hourNow === blockTime) {
+    if (hourNow === blockTime) {
         $(this).next().addClass("present").text(textToDo);
+        console.log(this);
     }
     else if (hourNow < blockTime) {
         $(this).next().addClass("future").text(textToDo);
@@ -26,18 +27,21 @@ $(".hour").each(function(){
         $(this).next().addClass("past").text(textToDo);
     }
 
-    console.log(blockTime);
-});
+    // double checking for type of blockTime.
+    //console.log(blockTime);
+})
 
 
 // save to local storage
 
 function saveToStorage(){
     var textToDo = $(this).prev().val();
-    //console.log(this);
+    console.log(this);
+    console.log(textToDo);
 
     var blockTime = $(this).prev().prev().attr("id");
     //console.log(this);
+    console.log(blockTime);
     localStorage.setItem(blockTime, textToDo);
 };
 
